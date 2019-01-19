c93a8effc55edf679591706381079678efc83172
Bogdan <boancas_bogdan@yahoo.com>
2019-01-19 13:52:52 +0200

    validare user si parola

--- app/src/main/java/bogdan/mobileapp/MainActivity.java
+++ app/src/main/java/bogdan/mobileapp/MainActivity.java
@@ -3,6 +3,7 @@ package bogdan.mobileapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
+    private int counter=5;
     /*/end*/
 
 
@@ -27,12 +29,31 @@ public class MainActivity extends AppCompatActivity {
         Info = (TextView)findViewById(R.id.tvInfo);
         Login= (Button)findViewById(R.id.btnLogin);
         /*end*/
-    }
 
-    privade void validate(String userName, String userPassword){
+
+        /*validam stringurile*/
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+/*daca user si parola sunt egale cu ce e definit, intram in "second activity'*/
+    private void validate(String userName, String userPassword){
         if((userName == "Admin") && (userPassword == "1234")){
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        /*daca nu dezactivam butonul de login cand variabila counter ajunge la 0*/
+        else{
+            counter--;
+            if(counter == 0){
+                Login.setEnabled(false);
 
+            }
         }
     }
 
