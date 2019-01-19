271ca748ad7dfd4f1c60132df0435b31e0d050e4
Bogdan <boancas_bogdan@yahoo.com>
2019-01-19 17:13:10 +0200

    aprofundare si refactorizare pentru permisii acmera

--- app/src/main/java/bogdan/mobileapp/ThirdActivity.java
+++ app/src/main/java/bogdan/mobileapp/ThirdActivity.java
@@ -1,10 +1,12 @@
 package bogdan.mobileapp;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -40,8 +42,51 @@ private static final int REQUEST_CAMERA=1;
         }
     private void requestPermissions(){
         ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String permission[], int grantResult[]){
+        switch(requestCode){
+            case REQUEST_CAMERA:
+                if(grantResult.length>0){
+                    boolean cameraAccepted = grantResult[0] ==PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted){
+                        Toast.makeText(ThirdActivity.this, "Permisie acceptata!", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(ThirdActivity.this, "Permisia nu a fost acceptata!", Toast.LENGTH_LONG).show();
+                        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                            if(shouldShowRequestPermissionRationale(CAMERA)){
+                                displayAlertMessage("Trebuie aprobate ambele permisii",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+
+                }
+            break;
+        }
+    }
+    public void displayAlertMessage(String message, DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(ThirdActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("Anulare", null)
+                .create()
+                .show();
 
     }
+
+
+
 
     @Override
     public void handleResult(Result result) {
