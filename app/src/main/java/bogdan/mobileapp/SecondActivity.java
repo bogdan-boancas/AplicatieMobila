94bb10df30160c50bc61e2c2917bb1c3f1db6a29
Bogdan <boancas_bogdan@yahoo.com>
2019-01-19 15:48:53 +0200

    activity 3 create si adaugat buton pentru a ajunge la QR

--- app/src/main/java/bogdan/mobileapp/SecondActivity.java
+++ app/src/main/java/bogdan/mobileapp/SecondActivity.java
@@ -1,7 +1,10 @@
 package bogdan.mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -11,7 +14,7 @@ public class SecondActivity extends AppCompatActivity {
     TextView tv1;
     String[] name = {"Q","R","S","C", "A", "N", "N", "E", "R"};
     int timer=0;
-
+Button btn_scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,21 @@ public class SecondActivity extends AppCompatActivity {
             }
         };
         th.start();
+
+        /*trecerea prin buton la activitatea 3 */
+
+btn_scan=(Button) findViewById(R.id.btScan);
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in2=new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(in2);
+            }
+        });
+
+
+
 
     }
 }
