e7eab3fd6346e33b0a7ff092e46dc459ef4b9987
Bogdan <boancas_bogdan@yahoo.com>
2019-01-19 13:41:05 +0200

    variabile, functie de verificare user si pass ca string si second activity creat

--- app/src/main/java/bogdan/mobileapp/MainActivity.java
+++ app/src/main/java/bogdan/mobileapp/MainActivity.java
@@ -1,13 +1,39 @@
 package bogdan.mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+/*cream variabilele necesare*/
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    /*/end*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*asignam variabilele la id-urile elementelor*/
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login= (Button)findViewById(R.id.btnLogin);
+        /*end*/
     }
+
+    privade void validate(String userName, String userPassword){
+        if((userName == "Admin") && (userPassword == "1234")){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        }
+    }
+
 }
---
+++ app/src/main/java/bogdan/mobileapp/SecondActivity.java
@@ -0,0 +1,15 @@
+package bogdan.mobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.logindemo.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
