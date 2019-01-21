1c8d60e9f0750eaf2bba44b9eaeac673e17f8376
Bogdan <boancas_bogdan@yahoo.com>
2019-01-21 09:25:20 +0200

    afisare rezultat qr

--- app/src/main/java/bogdan/mobileapp/ThirdActivity.java
+++ app/src/main/java/bogdan/mobileapp/ThirdActivity.java
@@ -133,6 +133,7 @@ private static final int REQUEST_CAMERA=1;
                 startActivity(intent);
             }
         });
+        /*rezultate scanare qr code*/
         builder.setMessage(scanResult);
         AlertDialog alert=builder.create();
         alert.show();
